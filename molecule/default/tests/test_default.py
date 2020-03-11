73998facefe163ae82363855cb23d7d1b81e6081
Bodo Schulz <bodo.schulz@tallence.com>
2020-03-11 15:35:44 +0100

    new tests
    tests for storage_schema

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -53,9 +53,46 @@ def test_files(host, files):
     assert f.is_file
 
 
+def test_carbon_config(host, get_vars):
+    config_file = "/etc/go-carbon/go-carbon.conf"
+    content = host.file(config_file).content_string
+    assert 'data-dir = "%s"' % ( get_vars['go_carbon_whisper_data_directory'] ) in content
+
+
+def test_storage_schemas(host):
+
+    config_file = "/etc/go-carbon/storage-schemas.conf"
+    content = host.file(config_file).content_string
+
+    assert '[test]' in content
+    assert 'retentions = 5m:30d' in content
+
+
 def test_user(host):
     assert host.group("carbon").exists
     assert host.user("carbon").exists
     assert "carbon" in host.user("carbon").groups
     assert host.user("carbon").shell == "/sbin/nologin"
     assert host.user("carbon").home == "/home/graphite"
+
+
+def test_solr_service(host):
+    service = host.service("go-carbon")
+
+    # if( service.__class__.__name__ != 'SysvService' ):
+    assert service.is_enabled == True
+    assert service.is_running == True
+
+
+@pytest.mark.parametrize("ports", [
+    '0.0.0.0:8081',
+    '0.0.0.0:2003',
+    '0.0.0.0:7002',
+    '0.0.0.0:7003'
+])
+def test_open_port(host, ports):
+    for i in host.socket.get_listening_sockets():
+        print( i )
+
+    solr = host.socket("tcp://{}".format(ports))
+    assert solr.is_listening
